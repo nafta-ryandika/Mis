4587f8b18a7c5cff8b4d7405b1f50885752ecf31
nafta.ryandika <naftaryandikaisyacafahmi@gmail.com>
2024-01-29 11:32:06 +0700

    security exit permit

--- application/controllers/Hrd.php
+++ application/controllers/Hrd.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Exit_permit extends CI_Controller
+class Hrd extends CI_Controller
 {
     public function __construct()
     {
